2a8bcfdda8043dabb70115d5bf4feb1c27e589cf
uta7 <122001807+uta7@users.noreply.github.com>
2023-09-21 18:22:39 +0900

    Add files via upload

---
+++ kadai_007_new/associative.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>課題007</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         $data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+ 
+         print_r($personal_data);
+         ?>
+     </p>
+ </body>
+ 
+ </html>
